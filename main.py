6ea867c774d3c9858d8706b47be2eb1cc4f737a3
Shuhao Dong <me@shuhao.ca>
2023-03-24 16:55:58 +0000

    add api for getting all articles

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+import os
 from flask import Flask
+import pymongo
+import json
+
+db_password = os.environ['DB_PASSWORD']
+client = pymongo.MongoClient(
+    f"mongodb+srv://mindfulness:{db_password}@mindfulness-app.edmj9uo.mongodb.net/?retryWrites=true&w=majority"
+)
+db = client.mindfulness
+# collection = db.articles
 
 app = Flask(__name__)
 
@@ -6,6 +16,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello from Flask!'
+
+
+@app.route('/articles/')
+def articles():
+    all_articles = []
+    for entry in db.articles.find():
+        all_articles.append(entry)
+    return json.dumps(all_articles, default=str)
 
 
 app.run(host='0.0.0.0', port=81)
